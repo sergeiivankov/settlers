be6f125f228b98dab129e8a8cf59ecbdea5df372
Sergei Ivankov <sergeiivankov@pm.me>
2023-03-01 21:18:59 +0300

    Move public resources directory to client folder, generation html file with public resources cache clean, fix packets descriptions

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,14 +1,31 @@
+import { readFileSync } from 'fs';
+import copy from 'rollup-plugin-copy';
 import sass from 'rollup-plugin-sass';
+
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8'))
 
 export default [
 	{
 		input: 'src/main.js',
 		output: {
-			file: '../public/js/main.js',
+			file: 'dist/js/main.js',
 			format: 'iife'
 		},
 		plugins: [
-			sass({ output: '../public/style.css' })
+			sass({ output: 'dist/css/style.css' }),
+			copy({
+				targets: [
+					{
+						src: ['static/*', '!*.html'],
+						dest: 'dist'
+					},
+					{
+						src: 'static/*.html',
+						dest: 'dist',
+						transform: contents => contents.toString().replaceAll('{version}', pkg.version)
+					}
+				]
+			})
 		]
 	}
 ];
